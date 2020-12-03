20d335b17b87d6432881ea39a66142f688db813d
fedann <daniele99@ferrazzo.it>
2020-12-03 19:16:28 +0100

    📦fi bug during stop command

--- bot.py
+++ bot.py
@@ -21,7 +21,6 @@ with open(config_path) as configFile:
 def enqueue_output(out, queue):
     for line in iter(out.readline, b''):
         queue.put(line)
-    out.close()
 
 
 def out_log():
@@ -53,39 +52,15 @@ def start_server(message):
                 args = config['args']
                 serverDir = config['server-directory']
                 jarName = config['jar-name']  
-                process = subprocess.Popen(' '.join(['java', *args , '-jar' , jarName , 'nogui']),stdin=subprocess.PIPE, stdout=subprocess.PIPE,stderr=subprocess.PIPE ,  shell=True, cwd=serverDir)              
+                process = subprocess.Popen(['java', *args , '-jar' , jarName , 'nogui'],stdin=subprocess.PIPE, stdout=subprocess.PIPE,stderr=subprocess.PIPE , cwd=serverDir)                     
                 q = Queue()
                 t = Thread(target=enqueue_output, args=(process.stdout, q))
                 t.daemon = True
-                t.start()
-            
+                t.start()            
             else:
                 bot.reply_to(message, "the server is already up, type /restart to restart the server , /info to view the stdout or /stop to stop the server")
     except Exception  as e:
         print('Error in start ' + str(e)) 
-
-
-@bot.message_handler(commands=['info','i'])
-def info_server(message):
-    global process
-    global q
-    try:
-        if message.from_user.username in config['whitelist']:
-            if process is None:
-                bot.reply_to(message, "Server is not yet started! Use /start to start the server")
-            else:
-                out = out_log()
-                if not out:
-                    bot.reply_to(message, 'No log yet!')
-                else:
-                    string = ''.join(out)
-                    numoftime = len(string)//4096 + 1 #had to split the message if its too long
-                    for i in range(1,numoftime +1):
-                        part = out[len(out)//numoftime*(i-1):len(out)//numoftime*i]
-                        bot.reply_to(message,''.join(part))                       
-    except Exception  as e:
-        print('Error in info ' + str(e)) 
-
 
 @bot.message_handler(commands=['stop'])
 def stop_server(message):
@@ -101,34 +76,37 @@ def stop_server(message):
                     process.stdin.write(b'stop\n')
                     process.stdin.flush()
                     process.communicate()
-                    process.terminate()
-                else:
-                    process.terminate()
-                q = None
-                t = None
+                process.terminate()
                 process = None
+                t = None
+                q = None
                 bot.reply_to(message, 'Server Stopped, type /start to start the server')
     except Exception  as e:
         print('Error in stop ' + str(e))
-        bot.reply_to(message, 'Unable to stop the server. Try again /stop') 
+        bot.reply_to(message, 'Unable to stop the server. Try again /stop or contact admin') 
 
-@bot.message_handler(commands=['help_minecraft'])
-def help_command(message):
+
+@bot.message_handler(commands=['output','o'])
+def info_server(message):
     global process
-    global t
     global q
     try:
         if message.from_user.username in config['whitelist']:
             if process is None:
                 bot.reply_to(message, "Server is not yet started! Use /start to start the server")
-            elif not process.stdin.closed :           
-                process.stdin.write(b'help\n')
-                process.stdin.flush()
-                bot.reply_to(message, 'Help done, press /info to se result')
             else:
-                bot.reply_to(message, 'Unable to use this command right now, /stop the server and /start again')
+                out = out_log()
+                if not out:
+                    bot.reply_to(message, 'No output yet!')
+                else:
+                    string = ''.join(out)
+                    numoftime = len(string)//4096 + 1 #has to split the message if its too long
+                    for i in range(1,numoftime +1):
+                        part = out[len(out)//numoftime*(i-1):len(out)//numoftime*i]
+                        bot.reply_to(message,''.join(part))
     except Exception  as e:
-        print('Error in help ' + str(e))  
+        print('Error in info ' + str(e)) 
+
 
 
 @bot.message_handler(commands=['command'])
